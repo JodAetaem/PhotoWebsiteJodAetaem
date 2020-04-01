aaf40b9264f3f20bf9b5f0bf048044b4eaa39e79
JodAetaem <xx>
2020-04-02 00:49:58 +0200

    Add collection.json dynamically changing the collection button on the navbar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import collections from '../../assets/Collections.json';
+
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
+  allCollection = collections.collection;
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
