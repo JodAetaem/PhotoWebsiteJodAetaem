28e6cfecf1fcd50d5a155d11ef3fdb069ad706f8
JodAetaem <xx>
2020-04-18 20:06:16 +0200

    Sync problem to fix, first bricks for autogenerating layout of collection

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
- // @ts-ignore
-import collections from '../../assets/Collections.json';
+import {ActivatedRoute, Router} from '@angular/router';
+// @ts-ignore
+import collectionFromJson from '../../assets/Collections.json';
 
 @Component({
   selector: 'app-collection',
@@ -10,29 +10,95 @@ import collections from '../../assets/Collections.json';
 })
 export class CollectionComponent implements OnInit {
   collection;
+  photoLayout = {};
 
   constructor(
-    private route: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.collection = collections.collection[this.getCollectionIdFromName(params.get('collectionId'))];
+    this.activatedRoute.paramMap.subscribe(params => {
+      try {
+        this.collection = collectionFromJson.collection[this.getCollectionIdFromName(params.get('collectionId'))];
+        setTimeout(() => {
+          this.photoLayout = this.initPhotoLayout(params.get('collectionId'));
+        }, 300);
+      } catch (e) {
+        this.router.navigate(['/pageNotFound']);
+      }
     });
+
   }
 
   private getCollectionIdFromName(name) {
     let id = 0;
-    for (const collection of collections.collection) {
+    for (const collection of collectionFromJson.collection) {
       if (collection.name === name) {
         return id;
       }
       id++;
     }
+    throw new Error(
+      'No collection found. The URL is probably wrong'
+    );
   }
 
-  testFunction() {
-
+  getLayoutFromPhotoName(photoName) {
+    return this.photoLayout[this.getCollectionIdFromName(photoName)];
   }
+
+  isHorizontal(image) {
+    return image.width > image.height;
+  }
+
+  isVertical(image) {
+    return image.width < image.height;
+  }
+
+  collectionToHVS(collectionName) {
+    const response = [];
+    let image;
+    const collectionToExplore = collectionFromJson.collection[this.getCollectionIdFromName(collectionName)];
+    for (const imageName of collectionToExplore.files) {
+      image = new Image();
+      image.src = '../../assets/Photos/' + collectionName + '/' + imageName;
+      if (this.isHorizontal(image)) {
+        response.push('H');
+      } else if (this.isVertical(image)) {
+        response.push('V');
+      } else {
+        response.push('S');
+      }
+    }
+    return response;
+  }
+
+  initPhotoLayout(collectionName) {
+    const pageLayout = {};
+    let collectioName: string;
+    let idImage;
+
+    // for (const allCollection of collectionFromJson.collection) {
+    //   collectionName = allCollection.name;
+    const val = this.collectionToHVS(collectionName);
+    console.log(collectionName);
+    console.log(val);
+    idImage = 0;
+    // for (const picture of allCollection.files) {
+    //   imageRestante = allCollection.files.length - idImage;
+    //   img = new Image();
+    //   img.src = '../../assets/Photos/' + collectioName + '/' + picture;
+    //   if (this.isHorizontal(img)) {
+    //     if (imageRestante === 2 && (allCollection.files.length % 2) === 0) {
+    //     }
+    //   }
+    //   idImage++;
+    // }
+    pageLayout[collectioName] = [[2, 1], [1, 1]];
+    // }
+    return pageLayout;
+  }
+
 }
