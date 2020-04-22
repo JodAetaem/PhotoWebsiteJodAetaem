1e548d80d02e65fcdb8d3c1db8c183f8d2333500
JodAetaem <xx>
2020-04-22 15:30:30 +0200

    Autogenerated collection layout v0.1, need to fix async for collectionToHVS function (pictures loading?)

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -24,7 +24,7 @@ export class CollectionComponent implements OnInit {
         this.collection = collectionFromJson.collection[this.getCollectionIdFromName(params.get('collectionId'))];
         setTimeout(() => {
           this.photoLayout = this.initPhotoLayout(params.get('collectionId'));
-        }, 300);
+        }, 1000);
       } catch (e) {
         this.router.navigate(['/pageNotFound']);
       }
@@ -76,28 +76,71 @@ export class CollectionComponent implements OnInit {
   }
 
   initPhotoLayout(collectionName) {
-    const pageLayout = {};
-    let collectioName: string;
+    const pageLayout = [];
     let idImage;
 
-    // for (const allCollection of collectionFromJson.collection) {
-    //   collectionName = allCollection.name;
-    const val = this.collectionToHVS(collectionName);
+    const collectionOrientation = this.collectionToHVS(collectionName);
+    let leftToTreat = 0;
     console.log(collectionName);
-    console.log(val);
+    console.log(collectionOrientation);
     idImage = 0;
-    // for (const picture of allCollection.files) {
-    //   imageRestante = allCollection.files.length - idImage;
-    //   img = new Image();
-    //   img.src = '../../assets/Photos/' + collectioName + '/' + picture;
-    //   if (this.isHorizontal(img)) {
-    //     if (imageRestante === 2 && (allCollection.files.length % 2) === 0) {
-    //     }
-    //   }
-    //   idImage++;
-    // }
-    pageLayout[collectioName] = [[2, 1], [1, 1]];
-    // }
+    while (idImage < collectionOrientation.length) {
+      leftToTreat = collectionOrientation.length - idImage;
+      if (leftToTreat <= 2) {
+        if (collectionOrientation[idImage] === 'V' && collectionOrientation[idImage + 1] === 'H') {
+          pageLayout.push([1, 2]);
+          pageLayout.push([2, 2]);
+        } else {
+          pageLayout.push([2, 2]);
+          pageLayout.push([1, 2]);
+        }
+        idImage = idImage + 2;
+      }
+      else if (leftToTreat === 4) {
+        if (collectionOrientation[idImage] === 'V' && collectionOrientation[idImage + 1] === 'V') {
+          pageLayout.push([1, 2]);
+          pageLayout.push([1, 2]);
+          pageLayout.push([1, 1]);
+          pageLayout.push([1, 1]);
+          idImage = idImage + 4;
+        } else if (collectionOrientation[idImage] === 'V' && collectionOrientation[idImage + 1] === 'H'){
+          pageLayout.push([1, 2]);
+          pageLayout.push([2, 1]);
+          pageLayout.push([1, 1]);
+          pageLayout.push([1, 1]);
+          idImage = idImage + 4;
+        } else{
+          pageLayout.push([2, 1]);
+          pageLayout.push([1, 2]);
+          pageLayout.push([1, 1]);
+          pageLayout.push([1, 1]);
+          idImage = idImage + 4;
+        }
+      }
+      else if (leftToTreat > 11) {
+        pageLayout.push([1, 1]);
+        pageLayout.push([1, 1]);
+        pageLayout.push([1, 1]);
+        pageLayout.push([1, 1]);
+        pageLayout.push([1, 1]);
+        pageLayout.push([1, 1]);
+        idImage = idImage + 6;
+      }
+      else {
+        if (collectionOrientation[idImage] === 'V' && collectionOrientation[idImage + 1] === 'H'){
+          pageLayout.push([1, 2]);
+          pageLayout.push([2, 1]);
+          pageLayout.push([2, 1]);
+          idImage = idImage + 3;
+        } else {
+          pageLayout.push([2, 1]);
+          pageLayout.push([1, 2]);
+          pageLayout.push([2, 1]);
+          idImage = idImage + 3;
+        }
+      }
+    }
+    console.log('pageLayout: ', pageLayout);
     return pageLayout;
   }
 
